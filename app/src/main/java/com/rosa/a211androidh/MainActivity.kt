60ca9e4405eed3bb992249b819935735c0ae42aa
rosacamelia8 <rosacamelia81@gmail.com>
2021-11-13 00:05:20 +0800

    id komponen, dan cara memanggil di kotlin

--- app/src/main/java/com/rosa/a211androidh/MainActivity.kt
+++ app/src/main/java/com/rosa/a211androidh/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rosa.a211androidh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+
+        simpanButton.setOnClickListener {
+            namaEditText.setText("")
+        }
+
+        batalButton.setOnClickListener {
+            finish()
+        }
     }
 }
