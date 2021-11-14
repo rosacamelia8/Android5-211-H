ceb5e0c265500d7715ab01c00924dc6b3b4d2e06
rosacamelia8 <rosacamelia81@gmail.com>
2021-11-14 23:19:59 +0800

    EditText, val dan var

--- app/src/main/java/com/rosa/a211androidh/MainActivity.kt
+++ app/src/main/java/com/rosa/a211androidh/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rosa.a211androidh
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,16 +9,25 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val simpanButton = findViewById<Button>(R.id.simpanButton)
         val batalButton = findViewById<Button>(R.id.batalButton)
+
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val noHandphoneEditText = findViewById<EditText>(R.id.noHandphoneEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+        val jarakEditText = findViewById<EditText>(R.id.jarakEditText)
 
         simpanButton.setOnClickListener {
-            Toast.makeText(this, "Hai ",Toast.LENGTH_SHORT).show()
+            val nama = namaEditText.text.toString()
+            var jarak = jarakEditText.text.toString().toInt()
+            jarak = jarak * 1000
+            Toast.makeText(this, "Hai $jarak",Toast.LENGTH_SHORT).show()
         }
 
         batalButton.setOnClickListener {
