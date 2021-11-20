d50f33919c7ca8c1adc96796d6596cada117f140
rosacamelia8 <rosacamelia81@gmail.com>
2021-11-20 23:49:06 +0800

    mengirim data menggunakan intent

--- app/src/main/java/com/rosa/a211androidh/MainActivity.kt
+++ app/src/main/java/com/rosa/a211androidh/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rosa.a211androidh
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -25,9 +26,22 @@ class MainActivity : AppCompatActivity() {
 
         simpanButton.setOnClickListener {
             val nama = namaEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val noHandphone = noHandphoneEditText.text.toString()
             var jarak = jarakEditText.text.toString().toInt()
             jarak = jarak * 1000
+            val tanggalLahir = tanggalLahirEditText.text.toString()
+
+
             Toast.makeText(this, "Hai $jarak",Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, HasilActivity::class.java)
+            intent.putExtra("nama",nama)
+            intent.putExtra("email",email)
+            intent.putExtra("noHandphone",noHandphone)
+            intent.putExtra("tanggalLahir",tanggalLahir)
+            intent.putExtra("jarak",jarak)
+            startActivity(intent)
         }
 
         batalButton.setOnClickListener {
